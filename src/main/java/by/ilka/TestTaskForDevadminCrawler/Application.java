772bb6cd94313716335e583996137b59b8e4c84c
Ilka <st.ilka@mail.ru>
2018-04-20 04:53:16 +0300

    Initial commit

---
+++ src/main/java/by/ilka/TestTaskForDevadminCrawler/Application.java
@@ -0,0 +1,18 @@
+package by.ilka.TestTaskForDevadminCrawler;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForDevadminCrawlerApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskForDevadminCrawlerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+	}
+}
